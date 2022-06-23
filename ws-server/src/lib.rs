78316fdc71ff0638cebca889419b03186be831f3
magine <magine233@gmail.com>
2022-06-23 20:05:30 +0800

    Add more response data from server for ws api

--- ws-server/src/lib.rs
+++ ws-server/src/lib.rs
@@ -1,17 +1,37 @@
-use axum::{
-    extract::{ws::Message, Extension, WebSocketUpgrade},
-    response::IntoResponse,
-    Json,
-};
-use dashmap::DashSet;
-use futures::{Sink, Stream, StreamExt};
 use std::sync::Arc;
-use tracing::warn;
-use ws_shared::{Msg, MsgData};
 
-#[derive(Debug, Default)]
+use axum::extract::ws::Message;
+use axum::extract::Extension;
+use axum::extract::WebSocketUpgrade;
+use axum::response::IntoResponse;
+use axum::Json;
+use dashmap::DashSet;
+use futures::Sink;
+use futures::SinkExt;
+use futures::Stream;
+use futures::StreamExt;
+use tokio::sync::broadcast;
+use tracing::warn;
+use ws_shared::Msg;
+use ws_shared::MsgData;
+use ws_shared::ServerResp;
+
+const CAPACITY: usize = 64;
+
+#[derive(Debug)]
 pub struct State {
     online_dids: DashSet<String>,
+    tx: broadcast::Sender<Arc<Msg>>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        let (tx, _rx) = broadcast::channel(CAPACITY);
+        Self {
+            online_dids: Default::default(),
+            tx,
+        }
+    }
 }
 
 impl State {
@@ -47,97 +67,186 @@ pub async fn list_handler(Extension(state): Extension<Arc<State>>) -> Json<Vec<S
 }
 
 async fn handle_socket<S>(socket: S, state: Arc<State>)
-where
-    S: Stream<Item = Result<Message, axum::Error>> + Sink<Message> + Send + 'static,
-{
-    let (mut _sender, mut receiver) = socket.split();
+where S: Stream<Item = Result<Message, axum::Error>> + Sink<Message> + Send + 'static {
+    let (mut sender, mut receiver) = socket.split();
+
+    let dids = &ServerResp::list(state.list(None));
+    let data = dids.try_into().unwrap();
+    if sender.send(Message::Text(data)).await.is_err() {
+        warn!("failed to send list response");
+        return;
+    };
+
     let joined_dids = Arc::new(DashSet::new());
+    let mut rx = state.tx.subscribe();
 
     let state1 = state.clone();
     let joined_dids1 = joined_dids.clone();
 
-    while let Some(Ok(data)) = receiver.next().await {
-        if let Message::Text(msg) = data {
-            handle_message(
-                msg.as_str().try_into().unwrap(),
-                state1.clone(),
-                joined_dids1.clone(),
-            )
-            .await;
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(data)) = receiver.next().await {
+            if let Message::Text(msg) = data {
+                handle_message(
+                    msg.as_str().try_into().unwrap(),
+                    state1.clone(),
+                    joined_dids1.clone(),
+                )
+                .await;
+            }
         }
+    });
+
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            let data = msg.as_ref().try_into().unwrap();
+            if sender.send(Message::Text(data)).await.is_err() {
+                warn!("failed to send message");
+                break;
+            }
+        }
+    });
+
+    // If any of the tasks fail, we need to shut down the other one
+    tokio::select! {
+        _v1 = &mut recv_task => send_task.abort(),
+        _v2 = &mut send_task => recv_task.abort(),
     }
 
     // This user has left. Should clean all data bound on it.
     warn!("Connection for {:?} closed", joined_dids);
-    state.leave(joined_dids);
+    state.leave(joined_dids.clone());
+
+    for did in joined_dids.iter() {
+        if let Err(e) = state.tx.send(Arc::new(Msg::leave(&did))) {
+            warn!("failed to send leave message: {e}");
+        }
+    }
 }
 
 async fn handle_message(msg: Msg, state: Arc<State>, joined_dids: Arc<DashSet<String>>) {
-    if msg.data == MsgData::Join {
-        state.join(&msg.did);
-        joined_dids.insert(msg.did);
+    let msg = match msg.data {
+        MsgData::Join => {
+            state.join(&msg.did);
+            joined_dids.insert(msg.did.to_string());
+            msg
+        }
+        MsgData::Leave => {
+            let dids: Arc<DashSet<String>> = Arc::new(DashSet::new());
+            dids.insert(msg.did.to_string());
+            state.leave(dids);
+            joined_dids.remove(&msg.did);
+            msg
+        }
+    };
+
+    if let Err(e) = state.tx.send(Arc::new(msg)) {
+        warn!("error sending message: {e}");
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use anyhow::Result;
     use fake_socket::*;
+    use ws_shared::ServerRespData;
+
+    use super::*;
 
     #[tokio::test]
-    async fn handle_join_and_disconnect_should_work() -> Result<()> {
-        let (client1, client2, state) = prepare_connections().await?;
+    async fn handle_join_and_leave_should_work() -> Result<()> {
+        let (mut client1, mut client2, state) = prepare_connections().await?;
 
-        assert_eq!(state.list(None).len(), 0);
-
-        let msg1 = &Msg::new("alice", MsgData::Join);
-        let msg2 = &Msg::new("bob", MsgData::Join);
-        let msg3 = &Msg::new("carol", MsgData::Join);
-        let msg4 = &Msg::new("dan", MsgData::Join);
-
+        let msg1 = &Msg::new("carol", MsgData::Join);
         client1.send(Message::Text(msg1.try_into()?))?;
-        client2.send(Message::Text(msg2.try_into()?))?;
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        assert_recv_msg(&mut client1, "carol", MsgData::Join).await?;
+        assert_recv_msg(&mut client2, "carol", MsgData::Join).await?;
+        assert_list_equal(state.clone(), vec!["alice", "bob", "carol"]);
+
+        let msg2 = &Msg::new("carol", MsgData::Leave);
+        client1.send(Message::Text(msg2.try_into()?))?;
+
+        assert_recv_msg(&mut client1, "carol", MsgData::Leave).await?;
+        assert_recv_msg(&mut client2, "carol", MsgData::Leave).await?;
         assert_list_equal(state.clone(), vec!["alice", "bob"]);
 
-        client1.send(Message::Text(msg3.try_into()?))?;
-        client2.send(Message::Text(msg4.try_into()?))?;
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        assert_list_equal(state.clone(), vec!["alice", "bob", "carol", "dan"]);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn handle_client_disconnect_should_work() -> Result<()> {
+        let (mut client1, mut client2, state) = prepare_connections().await?;
+
+        let msg1 = &Msg::new("carol", MsgData::Join);
+        client1.send(Message::Text(msg1.try_into()?))?;
+
+        assert_recv_msg(&mut client1, "carol", MsgData::Join).await?;
+        assert_recv_msg(&mut client2, "carol", MsgData::Join).await?;
+        assert_list_equal(state.clone(), vec!["alice", "bob", "carol"]);
 
         drop(client1);
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        assert_list_equal(state.clone(), vec!["bob", "dan"]);
-
-        drop(client2);
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        assert_eq!(state.list(None).len(), 0);
+        assert_recv_dids_msg(
+            &mut client2,
+            vec!["alice".to_string(), "carol".to_string()],
+            MsgData::Leave,
+        )
+        .await?;
+        assert_list_equal(state.clone(), vec!["bob"]);
 
         Ok(())
     }
 
     async fn prepare_connections() -> Result<(FakeClient<Message>, FakeClient<Message>, Arc<State>)>
     {
-        let (client1, socket1) = create_fake_connection();
-        let (client2, socket2) = create_fake_connection();
+        let (mut client1, socket1) = create_fake_connection();
+        let (mut client2, socket2) = create_fake_connection();
 
         let state = Arc::new(State::default());
 
-        // mimic server behavior
+        // mimic server behavior for client1
         let state1 = state.clone();
         tokio::spawn(async move {
             handle_socket(socket1, state1).await;
         });
 
+        // client1 will get empty dids list from server once it connected
+        assert_recv_resp(&mut client1, ServerRespData::List(vec![])).await?;
+
+        // the state of server will not change before client1 join
+        let dids = state.list(None);
+        assert_eq!(dids.len(), 0);
+
+        // client1 join, the server broadcast a join message
+        let msg1 = &Msg::new("alice", MsgData::Join);
+        client1.send(Message::Text(msg1.try_into()?))?;
+        assert_recv_msg(&mut client1, "alice", MsgData::Join).await?;
+
+        // check the state of server updated
+        assert_list_equal(state.clone(), vec!["alice"]);
+
+        // mimic server behavior for client1
         let state1 = state.clone();
         tokio::spawn(async move {
             handle_socket(socket2, state1).await;
         });
 
-        let dids = state.list(None);
-        assert_eq!(dids.len(), 0);
+        // client2 will get dids list from server
+        assert_recv_resp(
+            &mut client2,
+            ServerRespData::List(vec!["alice".to_string()]),
+        )
+        .await?;
 
+        // client2 join, the server broadcast a join message
+        let msg2 = &Msg::new("bob", MsgData::Join);
+        client2.send(Message::Text(msg2.try_into()?))?;
+        assert_recv_msg(&mut client1, "bob", MsgData::Join).await?;
+        assert_recv_msg(&mut client2, "bob", MsgData::Join).await?;
+
+        // check the state of server updated
+        assert_list_equal(state.clone(), vec!["alice", "bob"]);
+
+        // return the clients and state for further tests
         Ok((client1, client2, state))
     }
 
@@ -145,5 +254,55 @@ mod tests {
         let mut dids = state.list(None);
         dids.sort();
         assert_eq!(dids, expected);
+    }
+
+    async fn assert_recv_msg(
+        client: &mut FakeClient<Message>,
+        did: &str,
+        data: MsgData,
+    ) -> Result<()> {
+        if let Some(Message::Text(msg1)) = client.recv().await {
+            let msg = Msg::try_from(msg1.as_str())?;
+            assert_eq!(msg.did, did);
+            assert_eq!(msg.data, data);
+        }
+
+        Ok::<_, anyhow::Error>(())
+    }
+
+    async fn assert_recv_dids_msg(
+        client: &mut FakeClient<Message>,
+        dids: Vec<String>,
+        data: MsgData,
+    ) -> Result<()> {
+        let mut expected_dids = dids.clone();
+        expected_dids.sort();
+
+        let mut got_dids = vec![];
+
+        for _ in 0..expected_dids.len() {
+            if let Some(Message::Text(msg1)) = client.recv().await {
+                let msg = Msg::try_from(msg1.as_str())?;
+                got_dids.push(msg.did.clone());
+                assert_eq!(msg.data, data);
+            }
+        }
+
+        got_dids.sort();
+        assert_eq!(got_dids, expected_dids);
+
+        Ok::<_, anyhow::Error>(())
+    }
+
+    async fn assert_recv_resp(
+        client: &mut FakeClient<Message>,
+        data: ServerRespData,
+    ) -> Result<()> {
+        if let Some(Message::Text(msg1)) = client.recv().await {
+            let msg = ServerResp::try_from(msg1.as_str())?;
+            assert_eq!(msg.data, data);
+        }
+
+        Ok::<_, anyhow::Error>(())
     }
 }
--- ws-shared/src/lib.rs
+++ ws-shared/src/lib.rs
@@ -1,5 +1,8 @@
-use serde::{Deserialize, Serialize};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+
+use serde::Deserialize;
+use serde::Serialize;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Msg {
@@ -9,9 +12,58 @@ pub struct Msg {
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct ServerResp {
+    pub timestamp: u64,
+    pub data: ServerRespData,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum MsgData {
     Join,
+    Leave,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum ServerRespData {
+    List(Vec<String>),
+}
+
+impl Msg {
+    pub fn new(did: &str, data: MsgData) -> Self {
+        Msg {
+            did: did.into(),
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+            data,
+        }
+    }
+
+    pub fn join(did: &str) -> Self {
+        Msg::new(did, MsgData::Join)
+    }
+
+    pub fn leave(did: &str) -> Self {
+        Msg::new(did, MsgData::Leave)
+    }
+}
+
+impl ServerResp {
+    pub fn new(data: ServerRespData) -> Self {
+        ServerResp {
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+            data,
+        }
+    }
+    pub fn list(dids: Vec<String>) -> Self {
+        ServerResp::new(ServerRespData::List(dids))
+    }
 }
 
 impl TryFrom<&str> for Msg {
@@ -30,19 +82,18 @@ impl TryFrom<&Msg> for String {
     }
 }
 
-impl Msg {
-    pub fn new(did: &str, data: MsgData) -> Self {
-        Msg {
-            did: did.into(),
-            timestamp: SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs(),
-            data,
-        }
-    }
+impl TryFrom<&str> for ServerResp {
+    type Error = serde_json::Error;
 
-    pub fn join(did: &str) -> Self {
-        Msg::new(did, MsgData::Join)
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        serde_json::from_str(s)
+    }
+}
+
+impl TryFrom<&ServerResp> for String {
+    type Error = serde_json::Error;
+
+    fn try_from(msg: &ServerResp) -> Result<Self, Self::Error> {
+        serde_json::to_string(msg)
     }
 }
